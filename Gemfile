600c9aba27e4dda59c61d5d8093e7a1ecbdc1ae4
Thomonique Moore <thomoniquelmo@gmail.com>
2015-04-03 16:39:34 +0000

    Adding dropbox support to carrier wave

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
+gem 'carrierwave'
+gem 'carrierwave-dropbox'
+
 group :production do
 
 gem 'pg'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327215419) do
+ActiveRecord::Schema.define(version: 20150403131606) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150327215419) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
