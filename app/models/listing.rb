bf9124be077de2ae1231a74010607eddc1a2efea
Thomonique Moore <thomoniquelmo@gmail.com>
2015-04-08 17:56:09 +0000

    Added validations for users/listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,8 @@
 class Listing < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+  
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  
+  validates_presence_of :image
 end
