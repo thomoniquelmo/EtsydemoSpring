bc703df53b05c45b9224ca076dc138a0fc984e3f
Thomonique Moore <thomoniquelmo@gmail.com>
2015-04-29 19:43:36 +0000

    Added support for listing user link

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,6 @@ class Listing < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0 }
   
   validates_presence_of :image
+  
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408004909) do
+ActiveRecord::Schema.define(version: 20150429192414) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150408004909) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
